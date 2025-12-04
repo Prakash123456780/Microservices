86826ad9f9b0c42b162a58024361e5eb00e21bda
Prakash123456780 <gsprakash2192@gmail.com>
2025-12-04 12:52:02 +0530

    Add files via upload

---
+++ inventory-service/src/main/java/dev/arsalaan/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package dev.arsalaan.inventoryservice.repository;
+
+import dev.arsalaan.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
