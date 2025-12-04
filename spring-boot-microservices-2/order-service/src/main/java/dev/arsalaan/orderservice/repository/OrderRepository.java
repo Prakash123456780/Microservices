8c1d89680eb76109fe9baf76b839cec9b16259a1
Prakash123456780 <gsprakash2192@gmail.com>
2025-12-04 12:39:18 +0530

    Delete spring-boot-microservices-2 directory

--- spring-boot-microservices-2/order-service/src/main/java/dev/arsalaan/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.arsalaan.orderservice.repository;
-
-import dev.arsalaan.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
