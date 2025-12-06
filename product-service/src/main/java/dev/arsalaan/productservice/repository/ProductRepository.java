f70c2253c2b303baac4588a07180ae2181480662
Prakash123456780 <gsprakash2192@gmail.com>
2025-12-06 14:51:53 +0530

    Delete product-service directory

--- product-service/src/main/java/dev/arsalaan/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.arsalaan.productservice.repository;
-
-import dev.arsalaan.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
